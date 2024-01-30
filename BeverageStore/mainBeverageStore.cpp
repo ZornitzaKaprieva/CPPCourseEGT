a1de862bffc6ba01d28fdc1b47f847b315a26beb
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2024-01-30 16:50:51 +0200

    BeverageStore

--- BeverageStore/mainBeverageStore.cpp
+++ BeverageStore/mainBeverageStore.cpp
@@ -1,3 +1,19 @@
+/*Create a drink shop project.
+* For each drink there is the following information: name of the drink, price of the drink, quantity of the drink ordered, type of drink (wine, gin, whiskey, water, juice, iced tea, …)
+Create a function that can print the drink information.
+
+* There are two types of drinks in the store: alcoholic and non-alcoholic.
+- Alcoholic beverages have additional variables: volume and tax.
+Their price is determined by the following formula: CalculatePrice() > Quantity * (Price * Tax).
+- Non-Alcoholic beverages have additional variables: liters and city tax.
+Their price is determined by the following formula: CalculatePrice() -> Quantity * Price + CityTax.
+
+* Create an order class that displays the drinks ordered, the date, and the final amount of the order.
+* Create a user class that has a username, a list/vector of orders
+* Create a method to display information about how many drinks of each type were sold.
+* Create a method that returns the best customer/customers (with the most expensive order).
+*Create a method that returns all customers with orders containing a drink of a given type.
+* Create a method that returns the 10 customers with orders containing a drink of a given type with an order turnover above a given amount.*/
 
 #include "Drinks.h"
 #include "Alcoholic.h"
