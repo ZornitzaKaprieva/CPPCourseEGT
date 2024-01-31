1defa7de098b7ca1252ddd8c4a2ce3c8305d289c
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2024-01-31 22:22:47 +0200

    EvenOdd_homewrok

--- EvenOdd/EvenOdd.cpp
+++ EvenOdd/EvenOdd.cpp
@@ -1,8 +1,3 @@
-/*Write a C++ program that:
-Uses user-entered integers
-Checks whether a given number is even or odd
-If the number is even, "even" is written on the screen, if it is odd - "odd"*/
-
 #include <iostream>
 using std::cout;
 using std::endl;
---
+++ EvenOdd/Task.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/*Write a C++ program that:
+Uses user-entered integers
+Checks whether a given number is even or odd
+If the number is even, "even" is written on the screen, if it is odd - "odd"*/
