fd911e592f0b9a011437b582a3a62015cda2b0ac
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2024-02-01 11:57:27 +0200

    Airplane Reservation System

--- AirplaneReservationSystem/mainAirplaneReservationSystem.cpp
+++ AirplaneReservationSystem/mainAirplaneReservationSystem.cpp
@@ -1,8 +1,3 @@
-/*A small airplane has just purchased a computer for its new automated reservations system.
-* You have been asked to program the new system.
-* You are write a program to assign seats on each flight of the airline`s only plane (capacity: 10 seats)
-Make appropriate menu and messages.*/
-
 #include <iostream>
 #include "Airplane.h"
 using std::cout;
