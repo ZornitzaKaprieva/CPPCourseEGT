5904dd096dd07b0d57cc24aeb1034faa5a579e95
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2024-02-01 12:06:15 +0200

    BeverageStore

--- BeverageStore/mainBeverageStore.cpp
+++ BeverageStore/mainBeverageStore.cpp
@@ -1,20 +1,3 @@
-/*Create a drink shop project.
-* For each drink there is the following information: name of the drink, price of the drink, quantity of the drink ordered, type of drink (wine, gin, whiskey, water, juice, iced tea, …)
-Create a function that can print the drink information.
-
-* There are two types of drinks in the store: alcoholic and non-alcoholic.
-- Alcoholic beverages have additional variables: volume and tax.
-Their price is determined by the following formula: CalculatePrice() > Quantity * (Price * Tax).
-- Non-Alcoholic beverages have additional variables: liters and city tax.
-Their price is determined by the following formula: CalculatePrice() -> Quantity * Price + CityTax.
-
-* Create an order class that displays the drinks ordered, the date, and the final amount of the order.
-* Create a user class that has a username, a list/vector of orders
-* Create a method to display information about how many drinks of each type were sold.
-* Create a method that returns the best customer/customers (with the most expensive order).
-*Create a method that returns all customers with orders containing a drink of a given type.
-* Create a method that returns the 10 customers with orders containing a drink of a given type with an order turnover above a given amount.*/
-
 #include "Drinks.h"
 #include "Alcoholic.h"
 #include "NonAlcoholic.h"
@@ -60,10 +43,10 @@ int main()
 	p1.printPurchase();
 	p2.printPurchase();	
 
-	std::vector<Purchase*> listOfPurchases;
-	/*listOfPurchases.push_back(p1);
-	listOfPurchases.push_back(p2);*/
+	std::vector<Purchase> listOfPurchases;
+	listOfPurchases.push_back(p1);
+	listOfPurchases.push_back(p2);
 
-	User u1("Zornitza", listOfPurchases); //todo
+	//User u1("Zornitza", listOfPurchases); //todo
 
 }
